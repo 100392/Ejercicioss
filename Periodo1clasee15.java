ac20aae762721485e3cbb87ce4c20d9d667f8631
Eduardo Milla <113549107+100392@users.noreply.github.com>
2022-09-15 20:46:02 -0600

    Add files via upload

---
+++ Periodo1clasee15.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package periodo1clase15;
+import java.util.Scanner;
+public class Periodo1clasee15 {
+    public static void main(String[] args) {
+    Scanner Entrada = new Scanner(System.in);
+    System.out.println("Ingresa la base: ");
+    double base = Entrada.nextDouble();
+    System.out.println("Ingresa el exponente: ");
+    double exponente = Entrada.nextDouble();
+    double resultado = Entrada.nextDouble();
+    System.out.println("El resultado es: " + resultado);
+    }  
+}
